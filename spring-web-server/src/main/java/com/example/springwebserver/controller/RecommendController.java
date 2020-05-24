ffd33b00f7358603075a43e69068bc3dd604c3ae
limingyang <limingyang0314@qq.com>
2020-05-24 20:31:32 +0800

    fix recommend bugs

--- spring-web-server/src/main/java/com/example/springwebserver/controller/RecommendController.java
+++ spring-web-server/src/main/java/com/example/springwebserver/controller/RecommendController.java
@@ -2,10 +2,12 @@ package com.example.springwebserver.controller;
 
 
 import com.example.springwebserver.dataObject.BookDO;
+import com.example.springwebserver.enums.EmBusinessError;
 import com.example.springwebserver.exception.BusinessException;
 import com.example.springwebserver.response.CommonReturnType;
 import com.example.springwebserver.service.BookRecommendService;
 import com.example.springwebserver.service.BookService;
+import com.example.springwebserver.service.model.BookModel;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller("recommend")
 @RequestMapping("/recommend")
@@ -47,12 +51,31 @@ public class RecommendController {
     public CommonReturnType getRecommendByBookID(@RequestParam(name = "page") int page,
                                                  @RequestParam(name = "size") int size,
                                                  @RequestParam(name = "bookID") Long bookID) throws BusinessException {
-        return CommonReturnType.create(bookRecommendService.listBookRecommendByBookID(bookID,page,size));
+        List<BookModel> data = bookRecommendService.listBookRecommendByBookID(bookID,page,size);
+        if(data.size() == 0){
+            throw new BusinessException(EmBusinessError.BOOK_NOT_EXIST);
+        }
+        return CommonReturnType.create(data);
     }
 
+    /**
+     * 通过userID获取相关推荐
+     * @param page
+     * @param size
+     * @param userID
+     * @return
+     * @throws BusinessException
+     */
+    @ApiOperation("获取某用户的相关书籍推荐")
+    @GetMapping("/user")
+    @ResponseBody
     public CommonReturnType getRecommendByUserID(@RequestParam(name = "page") int page,
                                                  @RequestParam(name = "size") int size,
-                                                 @RequestParam(name = "UserID") Long bookID) throws BusinessException {
-        return CommonReturnType.create(bookRecommendService.listBookRecommendByBookID(bookID,page,size));
+                                                 @RequestParam(name = "userID") Long userID) throws BusinessException {
+        List<BookModel> data = bookRecommendService.listBookRecommendByUserID(userID,page,size);
+        if(data.size() == 0){
+            throw new BusinessException(EmBusinessError.BOOK_NOT_EXIST);
+        }
+        return CommonReturnType.create(data);
     }
 }
