d7198ec065cc67f6537937d1086751700304245d
limingyang <limingyang0314@qq.com>
2020-06-08 11:05:18 +0800

    admin

---
+++ spring-web-server/src/main/java/com/example/springwebserver/controller/ManageController.java
@@ -0,0 +1,14 @@
+package com.example.springwebserver.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("manage")
+@RequestMapping("/admin")
+@Slf4j
+@Api(tags = "管理后台接口", value = "提供基础后台管理的 Rest API")
+public class ManageController {
+
+}
