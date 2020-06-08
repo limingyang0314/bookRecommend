e1692793329361e3fa20e18fec4704bd860f22f8
limingyang <limingyang0314@qq.com>
2020-06-08 11:05:56 +0800

    admin controller

--- spring-web-server/src/main/java/com/example/springwebserver/controller/AdminController.java
+++ spring-web-server/src/main/java/com/example/springwebserver/controller/AdminController.java
@@ -5,10 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("manage")
+@Controller("admin")
 @RequestMapping("/admin")
 @Slf4j
 @Api(tags = "管理后台接口", value = "提供基础后台管理的 Rest API")
-public class ManageController {
+public class AdminController {
 
 }
